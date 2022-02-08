d5ba7766e17624761bea06b9a805f43985ecee85
Chad Wilson <chadw@thoughtworks.com>
2022-02-08 21:31:25 +0800

    Fix test so it is locale-agnostic

--- src/test/java/com/thoughtworks/gocd/authorization/ldap/validators/CertificateValidatorTest.java
+++ src/test/java/com/thoughtworks/gocd/authorization/ldap/validators/CertificateValidatorTest.java
@@ -22,6 +22,11 @@ import com.thoughtworks.gocd.authorization.ldap.model.ValidationError;
 import com.thoughtworks.gocd.authorization.ldap.model.ValidationResult;
 import org.junit.Test;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -30,6 +35,33 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CertificateValidatorTest {
+
+    private final String EXPIRED_CERT = "-----BEGIN CERTIFICATE-----\n" +
+            "MIIBnTCCAQagAwIBAgIIZ4GQMRkdfkYwDQYJKoZIhvcNAQEFBQAwETEPMA0GA1UECxMGc3lzdGVt\n" +
+            "MB4XDTE3MDgyOTA5MzkxMFoXDTE3MDgzMDA5MzkxMFowETEPMA0GA1UECxMGc3lzdGVtMIGfMA0G\n" +
+            "CSqGSIb3DQEBAQUAA4GNADCBiQKBgQCt9ocr5IinlQuYLsQvjGG1Uc0avTrmJflaucDFigYKmBru\n" +
+            "x5WYRIteaGHwdnFua0zM1X69Zbdo9zIA5GAh/6nMxY0m2DBVzxYFnYHpeW2YRKqx+W+vIcxHWMIM\n" +
+            "CJ7E/FQo4LjVTLHYyYewg417LHBT+fBnboghojylT5JiZZ4JrQIDAQABMA0GCSqGSIb3DQEBBQUA\n" +
+            "A4GBAEgX8+fG4wiGZ0aLRBvgY/rGcUjaVbqS/f88SaHOrXl/PcghG6sUXcszVcVOFFNKDTSvTiTf\n" +
+            "rozRC1QG/Gh6J5oXD3mZES7Z8AGKuWf9HYGKbBmBmFsHjBJVjN0z3xceHfrB2Xq0knjk2Ud16syy\n" +
+            "vAJAehC2qMaw2tKYN8f1frqd-----END CERTIFICATE-----";
+    private final ZonedDateTime EXPIRY_DATE = ZonedDateTime.of(2017, 8, 30, 9, 39, 0, 0, ZoneId.of("UTC"));
+
+    private final String VALID_CERT_TEXT = "-----BEGIN CERTIFICATE-----\n" +
+            "MIICwTCCAamgAwIBAgIEf41zDzANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQLEwZzeXN0ZW0wHhcN\n" +
+            "MTcwODI3MDgzOTQ1WhcNMzcwODIyMDgzOTQ1WjARMQ8wDQYDVQQLEwZzeXN0ZW0wggEiMA0GCSqG\n" +
+            "SIb3DQEBAQUAA4IBDwAwggEKAoIBAQCA8CFA15j9HE6NXk76041oQovq09JpV+a7ISMr6DTaC9/W\n" +
+            "87Snk8bJptj+gO9HTMBOEzXbgc09ZkEa9binjbY1hpKJf7VAnX42cddfKEbPHG28dDtDzX7jfiNe\n" +
+            "qGGTQOa46Ipowz7ZjvMaKp44aDvet4vxVpjp176eXnpxgBmki5d6AKX2dnqJKNY7pwpXuROJw8D6\n" +
+            "wrjSuf/VoyqnCttFKWb0FXAxkQlay6h8kHgTLxYFHGmcJLTktFa2CQQU4eZJOV4JPXNMhQJPWUBs\n" +
+            "sTcMKJIk3pjK6ekQfc20GxgckfRDRUCjk8AQ6flsSVU8uz4iVjNq/ap+ymChLXgdm5JLAgMBAAGj\n" +
+            "ITAfMB0GA1UdDgQWBBR9wSOVHa3+ta0IBJMq24kSHSsuETANBgkqhkiG9w0BAQsFAAOCAQEASplY\n" +
+            "QlSxKJWYK2pzfZQ7I87qDpZstm2cw5wIfCA9NLUZruODExexUfEXpUersG9IECxvhHrTI4JSN+Nk\n" +
+            "g8l25CZHuonO3Wkv9cEITBwdLeAjHGoPfXpkFbMZKeJC6erqwpsTlZbmoUFRE+F/b2+jN5AkJhKM\n" +
+            "MskcUxuDk2bJ9OIwJMkU7/8QoZMm5ecgQVPdVPe3DHetPD36db5o3j5uOcFDH/YghssR4J+qNMI3\n" +
+            "DHcOCshgBqGaaHLlBvzvBsNZAzWYWJ+3ydh5cJAyWwXnRp6xSaIpHesuABi2ipv0HN90SQQP0dAe\n" +
+            "bRgEaG0k5Hahzbtte3nXwdcJQmwD0vusbQ==\n" +
+            "-----END CERTIFICATE-----\n";
 
     @Test
     public void shouldValidateValidCertificate() throws Exception {
@@ -74,32 +106,6 @@ public class CertificateValidatorTest {
 
         final ValidationError validationError = result.allErrors().get(0);
         assertThat(validationError.key(), is("Certificate"));
-        assertThat(validationError.message(), startsWith("Invalid Certificate, expired on `8/30/17"));
+        assertThat(validationError.message(), startsWith("Invalid Certificate, expired on `" + EXPIRY_DATE.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))));
     }
-
-    private String EXPIRED_CERT = "-----BEGIN CERTIFICATE-----\n" +
-            "MIIBnTCCAQagAwIBAgIIZ4GQMRkdfkYwDQYJKoZIhvcNAQEFBQAwETEPMA0GA1UECxMGc3lzdGVt\n" +
-            "MB4XDTE3MDgyOTA5MzkxMFoXDTE3MDgzMDA5MzkxMFowETEPMA0GA1UECxMGc3lzdGVtMIGfMA0G\n" +
-            "CSqGSIb3DQEBAQUAA4GNADCBiQKBgQCt9ocr5IinlQuYLsQvjGG1Uc0avTrmJflaucDFigYKmBru\n" +
-            "x5WYRIteaGHwdnFua0zM1X69Zbdo9zIA5GAh/6nMxY0m2DBVzxYFnYHpeW2YRKqx+W+vIcxHWMIM\n" +
-            "CJ7E/FQo4LjVTLHYyYewg417LHBT+fBnboghojylT5JiZZ4JrQIDAQABMA0GCSqGSIb3DQEBBQUA\n" +
-            "A4GBAEgX8+fG4wiGZ0aLRBvgY/rGcUjaVbqS/f88SaHOrXl/PcghG6sUXcszVcVOFFNKDTSvTiTf\n" +
-            "rozRC1QG/Gh6J5oXD3mZES7Z8AGKuWf9HYGKbBmBmFsHjBJVjN0z3xceHfrB2Xq0knjk2Ud16syy\n" +
-            "vAJAehC2qMaw2tKYN8f1frqd-----END CERTIFICATE-----";
-
-    private String VALID_CERT_TEXT = "-----BEGIN CERTIFICATE-----\n" +
-            "MIICwTCCAamgAwIBAgIEf41zDzANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQLEwZzeXN0ZW0wHhcN\n" +
-            "MTcwODI3MDgzOTQ1WhcNMzcwODIyMDgzOTQ1WjARMQ8wDQYDVQQLEwZzeXN0ZW0wggEiMA0GCSqG\n" +
-            "SIb3DQEBAQUAA4IBDwAwggEKAoIBAQCA8CFA15j9HE6NXk76041oQovq09JpV+a7ISMr6DTaC9/W\n" +
-            "87Snk8bJptj+gO9HTMBOEzXbgc09ZkEa9binjbY1hpKJf7VAnX42cddfKEbPHG28dDtDzX7jfiNe\n" +
-            "qGGTQOa46Ipowz7ZjvMaKp44aDvet4vxVpjp176eXnpxgBmki5d6AKX2dnqJKNY7pwpXuROJw8D6\n" +
-            "wrjSuf/VoyqnCttFKWb0FXAxkQlay6h8kHgTLxYFHGmcJLTktFa2CQQU4eZJOV4JPXNMhQJPWUBs\n" +
-            "sTcMKJIk3pjK6ekQfc20GxgckfRDRUCjk8AQ6flsSVU8uz4iVjNq/ap+ymChLXgdm5JLAgMBAAGj\n" +
-            "ITAfMB0GA1UdDgQWBBR9wSOVHa3+ta0IBJMq24kSHSsuETANBgkqhkiG9w0BAQsFAAOCAQEASplY\n" +
-            "QlSxKJWYK2pzfZQ7I87qDpZstm2cw5wIfCA9NLUZruODExexUfEXpUersG9IECxvhHrTI4JSN+Nk\n" +
-            "g8l25CZHuonO3Wkv9cEITBwdLeAjHGoPfXpkFbMZKeJC6erqwpsTlZbmoUFRE+F/b2+jN5AkJhKM\n" +
-            "MskcUxuDk2bJ9OIwJMkU7/8QoZMm5ecgQVPdVPe3DHetPD36db5o3j5uOcFDH/YghssR4J+qNMI3\n" +
-            "DHcOCshgBqGaaHLlBvzvBsNZAzWYWJ+3ydh5cJAyWwXnRp6xSaIpHesuABi2ipv0HN90SQQP0dAe\n" +
-            "bRgEaG0k5Hahzbtte3nXwdcJQmwD0vusbQ==\n" +
-            "-----END CERTIFICATE-----\n";
 }
